d5291009ec74aa82e631d185576d6577f9e2c35a
louiechristie <post@louiechristie.com>
2014-06-09 16:53:03 +0100

    initial git commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  attr_accessible :bio, :email, :name, :video
+
+  validates :email, presence: true, uniqueness: true, length: { in: 2..255 }
+
+  validates :name, presence: true, length: { in: 2..255 }
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComedySeedApp::Application.config.secret_token = '79d40b69025d392e55da904ba45a7971648a53894f59bef3498b61f0b02e875f11127f7b3c509358745190d6d2b52158b5aa307dee7b4f870266a5e0bda3c2c4'
