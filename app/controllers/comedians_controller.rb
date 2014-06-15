c7db343cc61ca8e664b81f889bf50fe469fd7cad
louiechristie <post@louiechristie.com>
2014-06-15 13:29:51 +0100

    order comedians top rated

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -4,11 +4,15 @@ class ComediansController < ApplicationController
 
   def index
     @comedians = @q.result(:distinct => true)
+    @comedians_with_ratings = @comedians.select { |comedian| !comedian.avg_rating.nan? } 
+    @comedians_without_ratings = @comedians.select { |comedian| comedian.avg_rating.nan? } 
+    @comedians = @comedians_with_ratings.sort_by!{|comedian| comedian.avg_rating}.reverse!
+    @comedians = @comedians_with_ratings.concat @comedians_without_ratings
   end
 
   def show
     @comedian = Comedian.find params[:id]
-    @rating = current_user.rating.where(comedian_id: @comedian.id).first || Rating.new if current_user
+    @rating = (current_user.rating.where(comedian_id: @comedian.id).first || Rating.new) if current_user
   end
 
   def edit
