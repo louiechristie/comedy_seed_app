a10539f9816f71ec7dc58be58dc1c7fdb8007750
louiechristie <post@louiechristie.com>
2014-06-17 14:25:09 +0100

    styling responsive show comedian

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,18 @@ module ApplicationHelper
   def youtube_embed(youtube_url)
     return nil if youtube_url == nil
 
-  if youtube_url[/youtu\.be\/([^\?]*)/]
-    youtube_id = $1
-  else
     # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
-    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
-    youtube_id = $5
-  end
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
 
-  %Q{<iframe title="YouTube video player" width="320" height="240" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+    %Q{<div class="flex-video">
+          <iframe src="http://www.youtube.com/embed/#{youtube_id}" frameborder="0" allowfullscreen></iframe>
+    </div>}
+
   end
   
 end
